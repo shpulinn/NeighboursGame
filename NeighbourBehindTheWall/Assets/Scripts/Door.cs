cb15d6ff8153e88d55ad3841fd9ff914e1ab74f1
Nikita <shpulinn@mail.ru>
2024-05-28 16:11:13 +0200

    Door script created

---
+++ NeighbourBehindTheWall/Assets/Scripts/Door.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private Color highlitedColor;
+    [Space] [SerializeField] private Transform exitDoorPosition;
+
+    private Color _defaultColor;
+
+    private void Start()
+    {
+        _defaultColor = sprite.color;
+    }
+
+    public void Interact(Transform playerTransform)
+    {
+        playerTransform.position = exitDoorPosition.position;
+    }
+
+    private void OnMouseEnter()
+    {
+        sprite.color = highlitedColor;
+    }
+
+    private void OnMouseExit()
+    {
+        sprite.color = _defaultColor;
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
